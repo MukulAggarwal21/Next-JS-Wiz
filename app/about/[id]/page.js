a76ddbc67fa8db406d919b8bfce1493ac6573654
MUKUL <mukul.2226csit1115@kiet.edu>
2024-01-22 15:15:42 +0530

    Redirecting Concept

---
+++ app/about/[id]/page.js
@@ -0,0 +1,16 @@
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+const Id = ({params}) => {
+  if(params.id == 4){
+     redirect('/login')
+  }
+  
+  return (
+    <div>
+      {params.id}
+    </div>
+  )
+}
+
+export default Id
